b55fb7cdd679455a022e15e9b5768f97d766a4dc
Trent Shailer <trent.shailer@gmail.com>
2025-07-27 16:48:55 +1200

    feat: improve ergenomics of fetch

--- htmplate/src/assets/scripts/fetch.ts
+++ htmplate/src/assets/scripts/fetch.ts
@@ -1,5 +1,3 @@
-import { setHref } from "./redirect.ts";
-
 export type Problem = {
   pointer: string;
   detail: string;
@@ -7,15 +5,10 @@ export type Problem = {
 
 export type ServerResponse<T> =
   | { status: "ok"; body: T }
-  | { status: "clientError"; problems: Problem[] }
-  | { status: "somethingWentWrong" }
+  | { status: "badRequest"; problems: Problem[] }
+  | { status: "unauthenticated" }
+  | { status: "error" }
   | never;
-
-export type LogoutConfig = {
-  deleteTokenEndpoint: string;
-  loginHref: string;
-  additionalHeaders: Header[];
-};
 
 export type Header = [string, string];
 
@@ -26,8 +19,6 @@ export class FetchBuilder {
   #url: string;
   #additionalHeaders: Header[] | null = null;
   #body: object | null = null;
-  #logoutConfig: LogoutConfig | null = null;
-  #logoutShouldReturn: boolean = false;
 
   constructor(method: "GET" | "POST" | "PUT" | "DELETE", url: string) {
     this.#method = method;
@@ -44,20 +35,12 @@ export class FetchBuilder {
     return this;
   }
 
-  setLogout(logoutConfig: LogoutConfig | null, shouldReturn: boolean): FetchBuilder {
-    this.#logoutConfig = logoutConfig;
-    this.#logoutShouldReturn = shouldReturn;
-    return this;
-  }
-
   async fetch<T>(): Promise<ServerResponse<T>> {
     return await fetch(
       this.#method,
       this.#url,
       this.#additionalHeaders,
       this.#body,
-      this.#logoutConfig,
-      this.#logoutShouldReturn,
     );
   }
 }
@@ -67,8 +50,6 @@ export async function fetch<T>(
   url: string,
   additionalHeaders: Header[] | null,
   body: object | null,
-  logoutConfig: LogoutConfig | null,
-  logoutShouldReturn: boolean,
 ): Promise<ServerResponse<T>> {
   const headers = new Headers();
 
@@ -126,37 +107,14 @@ export async function fetch<T>(
       });
 
       return {
-        status: "clientError",
+        status: "badRequest",
         problems: body.problems ?? [],
       };
     }
     case 401: {
-      if (logoutConfig) {
-        await logout(logoutConfig, logoutShouldReturn);
-      }
-      break;
+      return { status: "unauthenticated" };
     }
   }
 
-  return { status: "somethingWentWrong" };
-}
-
-export async function logout(
-  config: LogoutConfig,
-  shouldReturn: boolean,
-): Promise<never> {
-  const token = localStorage.getItem(TOKEN_KEY);
-  await new FetchBuilder("DELETE", config.deleteTokenEndpoint).setHeaders(config.additionalHeaders)
-    .fetch();
-  localStorage.removeItem(TOKEN_KEY);
-
-  if (token) {
-    alert("Your session has expired");
-  }
-
-  const href = shouldReturn
-    ? `${config.loginHref}?redirect=${encodeURI(location.href)}`
-    : config.loginHref;
-
-  return await setHref(href);
+  return { status: "error" };
 }
